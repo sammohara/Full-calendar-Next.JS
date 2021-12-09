2c57ba920d8855c782f2ec377137e2c169da2204
sammohara <samuel@mohara.co>
2021-12-09 19:49:23 +0200

    1 file modified

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -1,10 +1,14 @@
-import Calendar from '@fullcalendar/react';
+import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import styles from './Fullcalendar.module.scss';
 
-export default function FullCalendar(props) {
+export default function Calendar(props) {
   return (
-    <Calendar {...props} plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+    <FullCalendar
+      {...props}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+    />
   );
 }
 
