959742f2c3efc21762b817aa0fdaa1059aed228c
sammohara <samuel@mohara.co>
2021-12-09 13:22:15 +0200

    1 file modified

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,7 @@
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
