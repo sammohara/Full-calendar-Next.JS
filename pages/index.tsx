26a7c30fe5d24bbed42af53a7a4814d2fa5b6ea8
sammohara <samuel@mohara.co>
2021-12-09 10:29:02 +0200

    2 files modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 import FullCalendar from '@fullcalendar/react';
 import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 
 import styles from '../styles/Home.module.css';
@@ -10,10 +11,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Full Calendar Next JS Demo</title>
+        <title>Full Calendar Next JS POC</title>
       </Head>
 
-      <main className={styles.main}>Hello World 2</main>
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
     </div>
   );
 }
