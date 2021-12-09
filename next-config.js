92456051457f45c898ccabd323cf41ad2f02f43c
sammohara <samuel@mohara.co>
2021-12-09 13:05:09 +0200

    3 files modified

--- next-config.js
+++ next-config.js
@@ -1,3 +1,15 @@
+/** @type {import('next').NextConfig} */
 // const withPlugins = require('next-compose-plugins');
 // const withSass = require('next-sass');
 // co
+const withTM = require('next-transpile-modules')([
+  '@fullcalendar/common',
+  '@babel/preset-react',
+  '@fullcalendar/common',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/interaction',
+  '@fullcalendar/react',
+  '@fullcalendar/timegrid',
+]);
+
+module.exports = withTM({});
