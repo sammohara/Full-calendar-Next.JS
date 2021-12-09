08f23011e06d089cf2cd64b04f40b46aa3bcef69
sammohara <samuel@mohara.co>
2021-12-09 18:18:39 +0200

    1 file added

---
+++ next-config.js
@@ -0,0 +1,24 @@
+// /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const withSass = require('next-sass');
+
+const withTM = require('next-transpile-modules')([
+  '@fullcalendar/common',
+  '@babel/preset-react',
+  '@fullcalendar/common',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/interaction',
+  '@fullcalendar/react',
+  '@fullcalendar/timegrid',
+]);
+
+module.exports = withTm({
+  // Custom webpack config
+});
+
+// module.exports = withPlugins([[withTM], [withSass]], {
+//   webpack: (config) => {
+//     console.info();
+//     return config;
+//   },
+// });
