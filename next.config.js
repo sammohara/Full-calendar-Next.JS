d14f34334b892e179297fbbc4ee58306713b5993
sammohara <samuel@mohara.co>
2021-12-10 13:38:36 +0200

    1 file modified

--- next.config.js
+++ next.config.js
@@ -17,3 +17,8 @@ module.exports = withTM({
 //     return config;
 //   },
 // });
+
+
+// https://www.davidangulo.xyz/how-to-use-fullcalendar-in-next-js/
+// https://codesandbox.io/s/fullcalendar-nextjs-10-forked-b1885?file=/next.config.js
+// https://spacebro.io/articles/fullcalendar-nextjs-10-higher
